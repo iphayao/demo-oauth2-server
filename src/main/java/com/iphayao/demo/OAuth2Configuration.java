11a9a37eed24eb783ef378abd317a9f599d36951
iphayao <phayaob@gmail.com>
2020-04-15 22:54:27 +0700

    Update Demo for OAuth2 Authorization server

---
+++ src/main/java/com/iphayao/demo/OAuth2Configuration.java
@@ -0,0 +1,71 @@
+package com.iphayao.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class OAuth2Configuration extends AuthorizationServerConfigurerAdapter {
+    private AuthenticationManager authenticationManager;
+    private DataSource dataSource;
+
+    public OAuth2Configuration(AuthenticationConfiguration authenticationConfiguration, DataSource dataSource) throws Exception {
+        this.authenticationManager = authenticationConfiguration.getAuthenticationManager();
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+        oauthServer.tokenKeyAccess("isAnonymous()").checkTokenAccess("isAuthenticated()");
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.jdbc(dataSource);
+    }
+
+    /*@Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("my_service")
+                .authorizedGrantTypes("authorization_code", "client_credentials")
+                .secret("{noop}clientSecret")
+                .authorizedGrantTypes("ROLE_TRUSTED_CLIENT")
+                .scopes("any");
+    }*/
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenStore(tokenStore())
+                .accessTokenConverter(accessTokenConverter())
+                .authenticationManager(authenticationManager);
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        //converter.setSigningKey("P@ssw0rd");
+        KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource("apikey.jks"), "P@ssw0rd".toCharArray());
+        converter.setKeyPair(keyStoreKeyFactory.getKeyPair("myapi"));
+
+        return converter;
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        //return new JwtTokenStore(accessTokenConverter());
+        return new JdbcTokenStore(dataSource);
+    }
+}
---
+++ src/test/java/com/iphayao/demo/DemoOauth2ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iphayao.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoOauth2ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
